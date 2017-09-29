57c47319dfb70c40b19209c4ff805c98f18c46fb
richa.shaurbh <richa.shaurabh@gmail.com>
2017-09-29 12:16:48 -0700

    update app.rb for rauting store list and brand list

--- app.rb
+++ app.rb
@@ -5,3 +5,17 @@ Dir[File.dirname(__FILE__) + '/lib/*.rb'].each { |file| require file }
 get('/') do
   erb(:index)
 end
+
+get('/store') do
+  @stores = Store.all()
+  erb(:store)
+end
+
+get('/brand') do
+  @brands = Brand.all()
+  erb(:brand)
+end
+
+post('/brand') do
+  erb(:index)
+end
