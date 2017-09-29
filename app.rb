f9e3baaaa6338f39aeaff9f45397833583aaf5d9
richa.shaurbh <richa.shaurabh@gmail.com>
2017-09-29 14:28:28 -0700

    add store_detail and brand_detail rauting in app and created store & brand page with add feature

--- app.rb
+++ app.rb
@@ -17,5 +17,29 @@ get('/brand') do
 end
 
 post('/brand') do
-  erb(:index)
+  brand_name = params.fetch('name')
+  brand_price = params.fetch('price')
+  brand = Brand.new({:name => brand_name, :price => brand_price})
+  brand.save()
+  redirect to('/brand')
+end
+
+post('/store') do
+  store_name = params.fetch('name')
+  store = Store.new({:name => store_name})
+  store.save()
+  redirect to('/store')
+end
+
+get ('/store/:id') do
+  store_id = params[:id].to_i
+  @store = Store.find(store_id)
+  @brands = Brand.all()
+  erb(:store_detail)
+end
+
+get ('/brand/:id') do
+  brand_id = params[:id].to_i
+  @brand = Brand.find(brand_id)
+  erb(:store_detail)
 end
